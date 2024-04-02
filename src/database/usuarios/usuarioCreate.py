c200a86a3c41e121f27ae3a44f6cef4833c8cca6
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-04-02 12:07:19 -0300

    arrumando identação do usuarioCreate

--- src/database/usuarios/usuarioCreate.py
+++ src/database/usuarios/usuarioCreate.py
@@ -7,11 +7,13 @@ def createUsuario(nome, endereco, rg):
         "endereco": endereco,
         "rg": rg
     }
-    
+
     try:
         database.usuario.insert_one(usuario)
         print()
         print("Usuario registrado com sucesso!")
         input()
     except:
+        print()
         print("! ERRO NA INSERÇÃO DE USUARIO !")
+        input()
