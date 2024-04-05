b3a3d50756d9261110fd1bf1cd4cae20f2e0b166
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-04-05 19:56:56 -0300

    adicionadno create ao vendedor

--- src/menu/vendedor/vendedorCreateMenu.py
+++ src/menu/vendedor/vendedorCreateMenu.py
@@ -1,3 +1,45 @@
+from database.vendedor.vendedorCreate import vendedorCreate
+from database.connection import database
+from bson.objectid import ObjectId
+
 def vendedorCreateMenu():
-    print("\nCriar Vendedor\n")
-    input()
+    nome = str(input("Digite um nome: "))
+    rg = str(input("Digite um rg: "))
+
+    produtos = []
+    execucao = True
+    
+    opcao = str(input("Deseja adicionar produtos? (s/n) "))
+    if opcao == "s":
+        while execucao:
+            produtoID = str(input("Digite o id do produto: "))
+            produtoID = ObjectId(produtoID)
+            produtoCompleto = database.produto.find_one({"_id": produtoID})
+            if produtoCompleto == None:
+                print()
+                print("Produto não encontrado!")
+                print()
+                return
+            else:
+                produtoNome = produtoCompleto["nome"]
+                produtoDescricao = produtoCompleto["descricao"]
+                produtoPreco = produtoCompleto["preco"]
+            produto = {
+                "id": produtoID,
+                "nome": produtoNome,
+                "descricao": produtoDescricao,
+                "preco": produtoPreco
+            }
+            produtos.append(produto)
+
+            opcao = str(input("deseja adicionar mais? (s/n) "))
+            if opcao != "s":
+                execucao = False
+
+    vendedor = {
+        "nome": nome.lower(),
+        "rg": rg.lower(),
+        "produtos": produtos
+    }
+
+    vendedorCreate(vendedor)
